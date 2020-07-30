bcb8fdc79238666251ebf02bfc75962f37c3bfd6
Gabe Zentner <gzentner@Gabe-Zentner-MY216B-MacBook-Air.local>
2020-07-29 23:29:21 -0400

    expanding settings

--- jt_rnaseq/app.R
+++ jt_rnaseq/app.R
@@ -17,42 +17,42 @@ ui <- fluidPage(navbarPage(
         tabPanel("Volcano plot", sidebarLayout(
             sidebarPanel(
                 selectInput(
-                    "volcanodataset",
+                    "volcano_dataset",
                     label = "Experiment",
                     choices = c("Whole animal",
                                 "Fat body"),
                     selected = "Whole animal"
                 ),
+                numericInput(
+                    "volcano_padj",
+                    label = "Adjusted p-value threshold",
+                    value = 0.05
+                ),
+                numericInput(
+                    "volcano_fc",
+                    label = "log2(fold change) threshold",
+                    value = 1
+                ),
                 sliderInput(
-                    "volcanopointsize",
+                    "volcano_pointsize",
                     label = "Point size",
                     min = 0.1,
                     max = 2,
                     value = 0.5
                 ),
                 sliderInput(
-                    "volcanoxlim",
+                    "volcano_xlim",
                     label = "X-axis limits",
                     min = -20,
                     max = 20,
                     value = c(-20,20)
                 ),
                 sliderInput(
-                    "volcanoylim",
+                    "volcano_ylim",
                     label = "X-axis limits",
                     min = 0,
                     max = 300,
                     value = c(0,300)
-                ),
-                numericInput(
-                    "volcanopadj",
-                    label = "Adjusted p-value threshold",
-                    value = 0.05
-                ),
-                numericInput(
-                    "volcanofc",
-                    label = "log2(fold change) threshold",
-                    value = 1
                 )
             ),
             mainPanel(plotOutput("volcano"),
@@ -62,21 +62,58 @@ ui <- fluidPage(navbarPage(
         tabPanel("MA plot", sidebarLayout(
             sidebarPanel(
                 selectInput(
-                    "madataset",
+                    "ma_dataset",
                     label = "Experiment",
                     choices = c("Whole animal",
                                 "Fat body"),
                     selected = "Whole animal"
                 ),
+                numericInput(
+                    "ma_padj",
+                    label = "Adjusted p-value threshold",
+                    value = 0.05
+                ),
+                numericInput(
+                    "ma_fc",
+                    label = "log2(fold change) threshold",
+                    value = 1
+                ),
                 sliderInput(
-                    "mapointsize",
+                    "ma_pointsize",
                     label = "Point size",
                     min = 0.1,
                     max = 2,
                     value = 0.5
+                ),
+                sliderInput(
+                    "ma_xlim",
+                    label = "X-axis limits",
+                    min = 0,
+                    max = 20,
+                    value = c(0,20)
+                ),
+                sliderInput(
+                    "ma_ylim",
+                    label = "X-axis limits",
+                    min = -20,
+                    max = 20,
+                    value = c(-20,20)
                 )
             ),
             mainPanel(plotOutput("maplot"))
+        )),
+        ## Table
+        tabPanel("Table", sidebarLayout(
+            sidebarPanel(
+                selectInput(
+                    "table_dataset",
+                    label = "Experiment",
+                    choices = c("Whole animal",
+                                "Fat body"),
+                    selected = "Whole animal"
+                )
+            ),
+            mainPanel(dataTableOutput("deg_table"))
         ))
     )),
     
@@ -116,42 +153,42 @@ ui <- fluidPage(navbarPage(
 server <- function(input, output) {
     
     output$volcano <- renderPlot({
-        data <- switch(input$volcanodataset,
+        data <- switch(input$volcano_dataset,
                        "Whole animal" = m_vs_c_res,
                        "Fat body" = mut_vs_het_res)
         
         data <- data %>%
             mutate(significance = case_when(
-                   log2FoldChange <= -input$volcanofc & 
-                       padj < input$volcanopadj ~ "downregulated",
-                   log2FoldChange >= input$volcanofc & 
-                       padj < input$volcanopadj ~ "upregulated",
+                   log2FoldChange <= -input$volcano_fc & 
+                       padj < input$volcano_padj ~ "downregulated",
+                   log2FoldChange >= input$volcano_fc & 
+                       padj < input$volcano_padj ~ "upregulated",
                    TRUE ~ "not significant")
                    )
         
         ggplot(data, aes(log2FoldChange,-log10(padj))) +
             theme_bw() +
             theme(panel.grid = element_blank()) +
-            geom_point(aes(color = significance), size = input$volcanopointsize) +
+            geom_point(aes(color = significance), size = input$volcano_pointsize) +
             scale_color_manual(values = c("blue", "gray", "magenta")) +
-            geom_vline(xintercept = -input$volcanofc, lty = 2) +
-            geom_vline(xintercept = input$volcanofc, lty = 2) +
-            geom_hline(yintercept = -log10(input$volcanopadj), lty = 2) +
-            xlim(input$volcanoxlim) +
-            ylim(input$volcanoylim)
+            geom_vline(xintercept = -input$volcano_fc, lty = 2) +
+            geom_vline(xintercept = input$volcano_fc, lty = 2) +
+            geom_hline(yintercept = -log10(input$volcano_padj), lty = 2) +
+            xlim(input$volcano_xlim) +
+            ylim(input$volcano_ylim)
     })
     
     output$volcanotable <- renderTable({
-        data <- switch(input$volcanodataset,
+        data <- switch(input$volcano_dataset,
                        "Whole animal" = m_vs_c_res,
                        "Fat body" = mut_vs_het_res)
         
         data <- data %>%
             mutate(significance = case_when(
-                log2FoldChange <= -input$volcanofc & 
-                    padj < input$volcanopadj ~ "downregulated",
-                log2FoldChange >= input$volcanofc & 
-                    padj < input$volcanopadj ~ "upregulated",
+                log2FoldChange <= -input$volcano_fc & 
+                    padj < input$volcano_padj ~ "downregulated",
+                log2FoldChange >= input$volcano_fc & 
+                    padj < input$volcano_padj ~ "upregulated",
                 TRUE ~ "not significant")
             ) %>%
             group_by(significance) %>%
@@ -159,25 +196,34 @@ server <- function(input, output) {
     })
     
     output$maplot <- renderPlot({
-        data <- switch(input$madataset,
+        data <- switch(input$ma_dataset,
                        "Whole animal" = m_vs_c_res,
                        "Fat body" = mut_vs_het_res)
         
         ggmaplot(
             data,
-            fdr = 0.05,
-            fc = 2,
-            size = input$mapointsize,
+            fdr = input$ma_padj,
+            fc = 2^input$ma_fc,
+            size = input$ma_pointsize,
             genenames = as.vector(data$geneid),
             top = 10,
-            font.label = c("bold", 10),
+            font.label = c("bold", 14),
             label.rectangle = T,
-            font.legend = "bold",
+            font.legend = c("bold", 14),
             font.main = "bold",
             palette = c("magenta", "blue", "gray"),
             ggtheme = theme_classic(),
-            select.top.method = "padj"
+            select.top.method = "padj",
+            xlim = input$ma_xlim,
+            ylim = input$ma_ylim
         )
+    })
+    
+    output$deg_table <- renderDataTable({
+        data <- switch(input$table_dataset,
+                       "Whole animal" = m_vs_c_res,
+                       "Fat body" = mut_vs_het_res) %>%
+            select(-change)
     })
 }
 
